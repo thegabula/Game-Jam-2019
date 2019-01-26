a4d3343bcc0e5f9721011c2cbcd0f38f7c5e33f6
Othaur <35857900+Othaur@users.noreply.github.com>
2019-01-26 12:37:36 -0600

    Terrain Block with noise

---
+++ PondTest/Assets/Scripts/NoiseManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseManager : MonoBehaviour
+{
+    public NoiseManager instance;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = new NoiseManager();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PondTest/Assets/Scripts/RollingTerrain.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingTerrain : MonoBehaviour
+{
+    public Mesh mesh;
+    public MeshFilter filter;
+    public MeshRenderer renderer;
+
+    public int width;
+    public int depth;
+    public int height;
+
+    public int widthDivs;
+    public int depthDivs;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i, j, index;
+        float y;
+
+        int heightScale = 5;
+        float detailScale = 5.0f;
+
+        mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] verticies = mesh.vertices; //new Vector3[width * depth] ;
+
+        index = 0;
+        for (i = 0; i < verticies.Length; i++)
+        {
+            verticies[i].y = Mathf.PerlinNoise((verticies[i].x  + this.transform.position.x)/detailScale, (verticies[i].z + this.transform.position.z)/detailScale)* heightScale;
+  
+        }
+
+        mesh.vertices = verticies;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        this.gameObject.AddComponent<MeshCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
