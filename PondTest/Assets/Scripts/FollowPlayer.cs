4b28688341a3544bf2f1559c5d30a2163fa08b93
Othaur <35857900+Othaur@users.noreply.github.com>
2019-01-27 14:19:37 -0600

    Title Screen and Follow indicator

---
+++ PondTest/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y+3, player.transform.position.z + 30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y+3, player.transform.position.z + 30);
+    }
+}
