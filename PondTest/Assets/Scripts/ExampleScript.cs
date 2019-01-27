1e5fa676ce95443b301b7f7fd1d949a65c944fce
Othaur <35857900+Othaur@users.noreply.github.com>
2019-01-27 11:23:44 -0600

    BugPrefab working

---
+++ PondTest/Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    float smooth = 5.0f;
+    float tiltAngle = 60.0f;
+    float rotationSpeed = 4.0f;
+    float m_Speed;
+    //bool restoreRotation = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fetch the Rigidbody component you attach from your GameObject
+        m_Rigidbody = GetComponent<Rigidbody>();
+        //Set the speed of the GameObject
+        m_Speed = 2f;
+    }
+
+    void Update()
+    {
+        // Smoothly tilts a transform towards a target rotation.
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+
+        Quaternion target = Quaternion.Euler(tiltAroundX, tiltAroundZ, 0);
+
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+        transform.Translate(0, 0, Time.deltaTime * m_Speed);
+    }
+}
---
+++ PondTest/Assets/Scripts/GlobalFlock.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFlock : MonoBehaviour
+{
+    public GameObject bugPrefab;
+    public static float sendboxSize = 1;
+
+    //public GameObject goalMarker;
+
+    static int numBugs = 5;
+    public static GameObject[] allBugs = new GameObject[ numBugs ];
+
+    public static Vector3 goalPos;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        goalPos = GenGoalPos();
+      //  goalMarker.transform.position = goalPos;
+    }
+    void Start()
+    {
+        for (int i=0; i < numBugs; i++) {
+
+            Vector3 pos = GenGoalPos();
+
+            allBugs[i] = Instantiate(bugPrefab, pos, Quaternion.identity);
+        }
+        //Debug.Log( allBugs.Length.ToString() );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Random.Range(0, 10000)<50)
+        {
+            goalPos = GenGoalPos();
+         //   goalMarker.transform.position = goalPos;
+        }
+    }
+
+    public Vector3 GenGoalPos()
+    {
+        float tempfloat = Random.Range(-sendboxSize, sendboxSize);
+
+        Vector3 temp  = new Vector3(Random.Range(-sendboxSize, sendboxSize) + transform.position.x,
+                                  Random.Range(-sendboxSize, sendboxSize) + transform.position.y,
+                                  Random.Range(-sendboxSize, sendboxSize) + transform.position.z);
+
+        return temp;
+    }
+}
